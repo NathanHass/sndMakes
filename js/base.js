21730c43925d9a2d6adb14b21cc2f09bc8edcc52
dheerja <dheerja.kaur@gmail.com>
2014-10-18 16:00:56 -0400

    plugged in server

--- js/base.js
+++ js/base.js
@@ -5,57 +5,59 @@
 
 $(function() {
 
-	// dummy data
-	var data = {"title":"Google's Nexus 6 is a giant phablet designed for Android Lollipop | The Verge", 
-				"headline":"Google's Nexus 6 is a giant phablet designed for Android Lollipop",
-				"description":"Google is today introducing the Nexus 6, the latest in its line of smartphones designed to show off the capabilities of new Android releases. This is the biggest Nexus phone that Google has...",
-				"image": "http://cdn1.vox-cdn.com/uploads/chorus_image/image/41948964/N6-moreeverything-1600.0.0_cinema_1200.0.jpg",
-				"fbHeadline":"Google's Nexus 6 is a giant phablet designed for Android Lollipop",
-				"fbDescription":"Google is today introducing the Nexus 6, the latest in its line of smartphones designed to show off the capabilities of new Android releases. This is the biggest Nexus phone that Google has...",
-				"fbImage":"http://cdn1.vox-cdn.com/uploads/chorus_image/image/41948964/N6-moreeverything-1600.0.0_cinema_1200.0.jpg",
-				"twitterHeadline":"Google's Nexus 6 is a giant phablet designed for Android Lollipop",
-				"twitterDescription":"Google is today introducing the Nexus 6, the latest in its line of smartphones designed to show off the capabilities of new Android releases. This is the biggest Nexus phone that Google has...",
-				"twitterImage":"http://cdn1.vox-cdn.com/uploads/chorus_image/image/41948964/N6-moreeverything-1600.0.0_cinema_1200.0.jpg"};
-	
-	// set core metadata from data
-	var title = data['title'], headline = data['headline'], description = data['description'], image = data['image'], url, domain, siteName;
-
-	// set facebook and twitter metadata
-	var fbHeadline = (fbHeadline === null) ? fbHeadline = title : fbHeadline = data['fbHeadline'];
-	var fbDescription = (fbDescription === null) ? fbDescription = description : fbDescription = data['fbDescription'];
-	var fbImage = (fbImage === null) ? fbImage = image : fbImage = data['fbImage'];
-	var twitterHeadline = (twitterHeadline === null) ? twitterHeadline = headline : twitterHeadline = data['twitterHeadline'];
-	var twitterDescription = (twitterDescription === null) ? twitterDescription = description : twitterDescription = data['twitterDescription'];
-	var twitterImage = (twitterImage === null) ? twitterImage = image : twitterImage = data['twitterImage'];
-
-	// additional metadata
-	//, domain, googleHeadline, googleDescription, googleImage;
+	var title, headline, description, image, fbHeadline, fbDescription, fbImage, twitterHeadline, twitterDescription, twitterImage, siteURL, displayURL, domain, siteName;
 
 	$('.url-input-btn').click(function(e) {
 
 		e.preventDefault();
+					
+		siteURL = $('.url-input').val();
 
-		// parse domain
-		url = $('.url-input').val();
-		if (url != null) { 
-			url = url.replace('http://','');
-			url = url.replace('www.','');
-			domain = url;
-			if (domain.indexOf('/') > 0) { domain = domain.substring(0, domain.indexOf('/'))}
-		}
+		$.ajax({
+			url: '/?url='+siteURL,
+			dataType: 'json',
+			success: function(data) {
+				if (data != null) {
 
-		// parse site name
-		if (title.indexOf('-') > 0 && title.length > title.indexOf('-')+3) { siteName = title.substring(title.indexOf('-')+2); }
-		else if (title.indexOf('|') > 0 && title.length > title.indexOf('-')+3) { siteName = title.substring(title.indexOf('|')+2); }
+					// set core metadata from data
+					title = data['title'], headline = data['headline'], description = data['description'], image = data['image'];
 
-		// set text fields
-		$('#title').val(title), $('#headline').val(headline), $('#description').val(description), $('#fbHeadline').val(fbHeadline), $('#fbDescription').val(fbDescription), $('#fbImage').val(fbImage), $('#twitterHeadline').val(twitterHeadline), $('#twitterDescription').val(twitterDescription), $('#twitterImage').val(twitterImage);
+					// set facebook and twitter metadata
+					fbHeadline = (data['fbHeadline'] != null) ? fbHeadline = data['fbHeadline'] : fbHeadline = title;
+					fbDescription = (data['fbDescription'] != null) ? fbDescription = data['fbDescription'] : fbDescription = description;
+					fbImage = (data['fbImage'] != null) ? fbImage = data['fbImage'] : fbImage = image;
+					twitterHeadline = (data['twitterHeadline'] != null) ? twitterHeadline = data['twitterHeadline'] : twitterHeadline = headline;
+					twitterDescription = (data['twitterDescription'] != null) ? twitterDescription = data['twitterDescription'] : twitterDescription = description;
+					twitterImage = (data['twitterImage'] != null) ? twitterImage = data['twitterImage'] : twitterImage = fbImage;
+					if (twitterImage == null) {twitterImage = ''}
+					
+					// parse domain
+					if (siteURL != null) { 
+						displayURL = siteURL.replace('http://','');
+						displayURL = displayURL.replace('www.','');
+						domain = displayURL;
+						if (domain.indexOf('/') > 0) { domain = domain.substring(0, domain.indexOf('/'))}
+					}
 
-		// update displays
-		updateFacebook();
-		updateTwitter();
-	
+					// parse site name
+					if (title.indexOf('-') > 0 && title.length > title.indexOf('-')+3) { siteName = title.substring(title.indexOf('-')+2); }
+					else if (title.indexOf('|') > 0 && title.length > title.indexOf('-')+3) { siteName = title.substring(title.indexOf('|')+2); }
+
+					// set text fields
+					$('#title').val(title), $('#headline').val(headline), $('#description').val(description), $('#fbHeadline').val(fbHeadline), $('#fbDescription').val(fbDescription), $('#fbImage').val(fbImage), $('#twitterHeadline').val(twitterHeadline), $('#twitterDescription').val(twitterDescription), $('#twitterImage').val(twitterImage);
+
+					// update displays
+					updateFacebook();
+					updateTwitter();
+				}
+			}
+		});	
 	});
+
+	$('.general-submit').click(function(e) {
+		e.preventDefault();
+		headline = $('#headline').val(), description = $('#description').val(), image = $('#image').val();
+	})
 
 	$('.fb-submit').click(function(e) {
 		e.preventDefault();
@@ -79,7 +81,7 @@ $(function() {
 	}
 
 	function updateTwitter() {
-		$('.twitter-desktop-title').html(twitterHeadline+' <a href="'+url+'">'+url.substring(0,27)+'...</a>');
+		$('.twitter-desktop-title').html(twitterHeadline+' <a href="'+siteURL+'">'+displayURL.substring(0,27)+'...</a>');
 		$('.twitter-desktop-img').attr('style', 'background-image: url('+twitterImage+');');
 	}	
 
